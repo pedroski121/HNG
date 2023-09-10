4f76c41e6c28a99c09c7c35243e835f23ac59456
Pedro Obi <obipedro121@gmail.com>
2023-09-10 15:01:36 +0100

    HNG task 1

---
+++ task_1/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import 'dotenv/config'
+
+
+const app = express();
+
+const PORT = parseInt(`${process.env.PORT}`) || 5000
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+app.get('/api', (req, res)=>{
+    const slack_name = req.query.slack_name
+    const track = req.query.track
+    const utcTime = new Date(Date.now()).toISOString()
+    const date = new Date()
+    const day = date.getDay()
+    const current_day = days[day]
+res.status(200).json({
+    slack_name,
+  current_day,
+  utc_time: utcTime,
+  track,
+  github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
+  github_repo_url: "https://github.com/username/repo",
+  status_code: 200
+    })
+})
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}`) 
+})
+
+
